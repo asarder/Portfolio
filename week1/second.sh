6bba8076dd98de0e3c5280e798ff8db3e208a821
asarder <mkabir73@gmail.com>
2022-02-09 02:02:56 +0000

    modifi

--- week1/second.sh
+++ week1/second.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+#Author: Abu Sarder
+#Date:24/01/22
+# understanding $? variable
 
 echo "Hi there"
 
-echo "It is good to see you $1 $2 $3"
+echo "It is $1 to see $2 $3" 
+# on these variable we can input desired words or numbers. 
+#Example: good, you, Abu
 
 exit 0
 
