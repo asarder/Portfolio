30e2258d6f81d4ab57a23df5aecf7f2fde01f215
asarder <mkabir73@gmail.com>
2022-02-27 06:40:43 +0000

    Portfolio

--- week1/foldermaker.sh
+++ week1/foldermaker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Author:Abu Sarder
+#Date 26/01/22
+#Creating folder and this folder will be transfered to week2
 
-read -p "type the folder name: " folderName
-mkdir "$folderName"
+read -p "Ender folder name you would like to create: " fold_name
+mkdir $fold_name  
