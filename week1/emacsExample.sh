caef42e2f18f42ebd9a9876f96b8486afc822be8
asarder <mkabir73@gmail.com>
2022-03-01 01:40:37 +0000

    first commit

--- week1/emacsExample.sh
+++ week1/emacsExample.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#Author: Abu Sarder
+#Date 23/01/2022
+#Begin Bash scripts
+
 echo "Hello from Emacs"
 exit 0
--- week1/vimExample.sh
+++ week1/vimExample.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+#Author: Abu Sarder
+#Date:24/01/22
+# understanding vim text editor
+
 echo "Hello from Vim"
 exit 0
